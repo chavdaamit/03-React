a007e3f548f92c552b4ae07ecee6db26b5f60953
Amit Chavda <adchavda7777@gmail.com>
2026-09-09 13:02:38 +0530

    State-hooks add

--- 03-state-hooks/src/components/01-state.jsx
+++ 03-state-hooks/src/components/01-state.jsx
@@ -3,11 +3,15 @@ import { useState } from "react";
 const State = () => {
   const [counter, setcounter] = useState(0);
 
+  // const User = () => {
+  //   setcounter(counter + 1);
+  // };
+
   return (
     <>
       <h1>{counter}</h1>
       <button onClick={() => setcounter(counter + 1)}>increse</button>
-      
+      {/* <button onClick={User} >hbhfvhb</button> */}
     </>
   );
 };
