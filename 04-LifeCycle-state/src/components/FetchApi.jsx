e181c383a3d7b45f56d276f15488d93ebcb96601
Amit Chavda <adchavda7777@gmail.com>
2026-09-11 15:32:47 +0530

    table changes

--- 04-LifeCycle-state/src/components/FetchApi.jsx
+++ 04-LifeCycle-state/src/components/FetchApi.jsx
@@ -3,14 +3,19 @@ import { useEffect, useState } from "react";
 const Fetch = () => {
   const [user, setUser] = useState([]);
 
+  const [Error, setError] = useState(null);
+
   useEffect(() => {
     async function fetchuser() {
       const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      if (!res.ok) {
+        setError("no data Found");
+      }
 
       const data = await res.json();
 
       console.log(data);
-      
+
       setUser(data);
     }
 
@@ -19,29 +24,35 @@ const Fetch = () => {
 
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>email</th>
-            <th>address</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {user.map((u) => {
-            return (
+      {Error != null ? (
+        <>
+          <h1>{Error}</h1>
+        </>
+      ) : (
+        <>
+          <table>
+            <thead>
               <tr>
-                <td>{u.id}</td>
-                <td>{u.name}</td>
-                <td>{u.email}</td>
-                <td>{u.address["city"]}</td>
+                <th>id</th>
+                <th>name</th>
+                <th>email</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+
+            <tbody>
+              {user.map((u) => {
+                return (
+                  <tr key={u.id}>
+                    <td>{u.id}</td>
+                    <td>{u.name}</td>
+                    <td>{u.email}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      )}
     </>
   );
 };
